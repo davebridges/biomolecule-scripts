6750c046f7083620aa0532b0e78a18c263e3c58c
davebridges <dave.bridges@gmail.com>
2012-10-18 11:35:49 -0400

    changed filtering to use awk to filter for chromosome ranges according to http://seqanswers.com/forums/showthread.php?t=6892

--- Shell/samtools.sh
+++ Shell/samtools.sh
@@ -3,5 +3,5 @@ samtools merge -f combined.bam */accepted_hits.bam
 samtools sort -f combined.bam combined.sorted
 samtools index combined.sorted.bam
 
-#filter a bam file for a particular region (look up region on Ensembl)
- samtools view combined.sorted.bam 2:209000000-210000000 > region_name.bam
+#filter a bam file for a particular chromosome set (see http://seqanswers.com/forums/showthread.php?t=6892)
+samtools view -h *sorted.bam | awk '$3=="chr1" || $3=="chr3" || /^@/' | samtools view -Sb -> onlychr1_3.bam.sorted.bam
