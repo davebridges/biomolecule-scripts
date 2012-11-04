cbde42fa1591f001cde448e3275d81cf3596163c
Dave Bridges <dave.bridges@gmail.com>
2012-11-04 18:50:50 -0500

    Documented options used in tophat2 and cufflinks.

--- RNAseq/tophat_cufflinks.sh
+++ RNAseq/tophat_cufflinks.sh
@@ -20,7 +20,9 @@ for sample in Sample_12100 Sample_12101 Sample_12102 Sample_12103 Sample_12104 S
 
 do
   #run tophat alignment
+  #this uses multiple processors (-p 11) and to first map to known sequences (-G) before matching other sequences
   echo "tophat2 -p 11 -G $GTF -o tophat_out/$sample $REFERENCE $sample.fa" > $sample.sh
+  #The options are to use multiple cores (-p 11), to do a soft RABT assembly (-g) and to specify the output directory (-o)
   echo "cufflinks -p 11 -u -g $GTF -o cufflinks_out/$sample tophat_out/$sample/accepted_hits.bam" >> $sample.$
   echo "cufflinks_out/$sample/transcripts.gtf" >> assemblies.txt
   qsub -cwd $sample.sh
