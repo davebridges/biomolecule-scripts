055c7caa34155d3b75eb6097547457c75cc6edd4
davebridges <dave.bridges@gmail.com>
2012-12-14 13:25:14 -0500

    Added samtools script for determining the number of multiply mapped reads.

--- RNAseq/Shell/samtools.sh
+++ RNAseq/Shell/samtools.sh
@@ -5,3 +5,6 @@ samtools index combined.sorted.bam
 
 #filter a bam file for a particular chromosome set (see http://seqanswers.com/forums/showthread.php?t=6892)
 samtools view -h *sorted.bam | awk '$3=="chr1" || $3=="chr3" || /^@/' | samtools view -Sb -> onlychr1_3.bam.sorted.bam
+
+#calculated multi-mapped reads from a bam file (see http://seqanswers.com/forums/showpost.php?p=60498&postcount=4)
+samtools view -F 4 file.bam | awk '{printf $1"\n"}' | sort | uniq -d | wc -l
