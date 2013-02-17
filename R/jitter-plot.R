f63a748ad1d299c48296da3d8235239660526101
Dave Bridges <dave.bridges@gmail.com>
2013-02-16 23:02:56 -0500

    Converted into a function.

--- R/jitter-plot.R
+++ R/jitter-plot.R
@@ -3,14 +3,19 @@ site <- "https://spark-public.s3.amazonaws.com/dataanalysis/movies.txt"
 download.file(site, destfile="movies.txt", method="curl")
 MovieData <- read.delim("movies.txt")
 
-with(MovieData, plot(jitter(as.numeric(rating)), score, pch=19, col=rating, xaxt="n"))
-axis(side=1, at=1:4, labels=levels(MovieData$rating))
+jitter.plot <- function(x,y,xlab=names(x)){
+plot(jitter(as.numeric(x)), y, pch=19, col=x, xaxt="n",xlab=xlab)
+axis(side=1, at=1:length(levels(x)), labels=levels(x))
 
 #calculate means
-means <- tapply(MovieData$score,MovieData$rating, mean)
+means <- tapply(y,x, mean)
 #add axis means
-scaling = length(levels(MovieData$rating))
+scaling = length(levels(x))
 scaling.values <- seq(1, scaling, by=1)
 scaling.values.upper <- scaling.values+1/scaling
 scaling.values.lower <- scaling.values-1/scaling
 for (i in scaling.values) segments(scaling.values.lower[i], means[i], scaling.values.upper[i], means[i], col=palette()[i])
+}
+
+#usage
+with(MovieData, jitter.plot(rating,score))
